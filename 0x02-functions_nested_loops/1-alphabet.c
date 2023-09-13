c17bbd92a21abcc11259c63ac095b0bbbc9be3d2
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-13 03:53:30 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - Prints the alphabet in lowercase
-*/
+ */
 void print_alphabet(void)
 {
-  char letter = 'a';
-  while (letter <= 'z')
-  {
-    
-    _putchar(letter);
-    letter++;
-  }
-  _putchar('\n');
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
