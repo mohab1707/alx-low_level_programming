57381a30e14f848036da75cdceb7209c380fe450
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-13 10:49:20 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+int row, column, result;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+result = row * column;
+if (column == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+if (result < 10)
+{
+_putchar(' ');
+}
+else
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
