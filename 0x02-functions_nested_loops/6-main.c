0efc1085f51e4d0e113be0ef8c3e2fcdb554184e
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-13 09:50:55 -0700

    6

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    _putchar(r);
+    _putchar('\n');
+    r = _abs(0);
+    _putchar(r);
+    _putchar('\n');
+    r = _abs(1);
+    _putchar(r);
+    _putchar('\n');
+    r = _abs(-98);
+    _putchar(r);
+    _putchar('\n');
+    return (0);
+}
