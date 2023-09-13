33ad72423edcd36b9b420c2e16d66dd6028d92e4
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-13 10:34:02 -0700

    8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,18 @@
 int print_last_digit(int n)
 {
 int last_digit;
+if (n == INT_MIN)
+{
+last_digit = 8;
+}
+else
+{
 if (n < 0)
 {
-n = -n;
+  n = -n;
 }
 last_digit = n % 10;
+}
 _putchar('0' + last_digit);
 return (last_digit);
 }
