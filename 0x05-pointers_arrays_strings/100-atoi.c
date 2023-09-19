10fe8756b15bf4ee00e5a5c5cbd0dcfde54ae5a5
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-19 01:13:11 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -8,8 +9,8 @@
  */
 int _atoi(char *s)
 {
-nt result = 0;
-int sign = 1; 
+int result = 0;
+int sign = 1;
 int i = 0;
 while (s[i] == ' ')
 {
@@ -17,12 +18,16 @@ i++;
 }
 if (s[i] == '-')
 {
-sign = -1; 
+sign = -1;
 i++;
 }
 else if (s[i] == '+')
 {
 i++;
+}
+if (s[i] < '0' || s[i] > '9')
+{
+return 0;
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
