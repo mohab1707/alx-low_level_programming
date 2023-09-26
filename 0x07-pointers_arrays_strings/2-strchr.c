4287b0b67f473b6847e3154a190cef4becf934cd
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-09-26 05:30:13 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,9 +5,9 @@
  * @s: Pointer to the string to search.
  * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence 
- *        of the character `c` in the string `s`,
- *        or NULL if the character is not found.
+ * Return: A pointer to the first occurrence
+ * of the character `c` in the string `s`,
+ * or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
