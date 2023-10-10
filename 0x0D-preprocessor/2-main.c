99fcef70fc645d29f68a0bf77d679fd0a8349a1f
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-10-10 12:51:28 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
 /**
-  * main function
-  */
+ * main - Entry point
+ *
+ * Description: Prints the name
+ * of the file it was compiled from
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 printf("%s\n", __FILE__);
