d6be6f5ddd52e03894558cf2fb98ec6971b9ccd6
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-10-10 13:14:48 -0700

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer to the struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
+}
