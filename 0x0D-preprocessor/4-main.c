6c724564fe993b12a2360cf0940da5ef7ccf53a0
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-10-10 12:58:16 -0700

    4

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Tests the SUM macro
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
