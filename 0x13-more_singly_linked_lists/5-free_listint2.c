a17002f428895e36a3d1c52d4cbddf408d50ae31
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-10-23 12:15:59 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,8 +3,10 @@
 
 /**
  * free_listint2 - Frees a
- * listint_t list and sets the head to NULL.
- * @head: A pointer to a pointer to
+ * listint_t list and sets
+ * the head to NULL.
+ * @head: A pointer to a
+ * pointer to
  * the head of the list.
  */
 void free_listint2(listint_t **head)
